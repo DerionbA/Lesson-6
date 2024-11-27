012872b09c072142ce74ec16fb942fc95306b575
DerionbA <alemdavis8@gmail.com>
2024-11-27 17:23:30 -0400

    all shapes in one

---
+++ Square on a Canvas.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.Screen().bgcolor("orange")
+sc = turtle.Screen()
+sc.setup(400,300)
+turtle.title("welcone to Derion turtle ")
+boart = turtle.Turtle()
+
+
+for i in range(4):
+    boart.forward(100)
+    boart.left(90)
+    i = i + 1 
---
+++ Star.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.Screen().bgcolor("orange")
+boart = turtle.Turtle()
+boart.forward(100)
+boart.left(120)
+boart.forward(100)
+boart.left(120)
+boart.forward(100)
+boart.penup()
+boart.right(150)
+boart.forward(50)
+
+boart.pendown()
+boart.right(90)
+boart.forward(100)
+boart.right(120)
+boart.forward(100)
+boart.right(120)
+boart.forward(100)
+
+turtle.done()
